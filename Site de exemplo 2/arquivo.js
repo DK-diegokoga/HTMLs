4f625209c8e8470e98278e4a851e07db6ece22ad
DK-diegokoga <diegokoga1@gmail.com>
2021-11-20 21:27:06 -0300

    first commit

---
+++ Site de exemplo 2/arquivo.js
@@ -0,0 +1,21 @@
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+function mensagem1(){
+    alert("Primeira mensagem");
+}
+function mensagem2(){
+    alert("Segunda mensagem");
+}
+function mensagem3(){
+    alert("Terceira mensagem");
+}
+function mensagem4(){
+    alert("Quarta mensagem");
+}
+function mensagem5(){
+    alert("Quinta mensagem");
+}
